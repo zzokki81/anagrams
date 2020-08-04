52d0a3ddbf55191e992a938f15082836f66bcfbb
zzokki81 <zoran.rebic@outlook.com>
2020-08-04 18:58:07 +0200

    Change find_anagram method input

--- findanagram.rb
+++ findanagram.rb
@@ -4,11 +4,11 @@ text_file, *input_word = ARGV
 
 input_word.each do |word|
   anagram = SearchAnagrams.new(word)
-  anagram_result = anagram.find_anagram('AnagramFile.txt')
+  anagram_result = anagram.find_anagram(text_file)
   if anagram_result == []
     puts("No anagrams found for word '#{word}'")
   else
-    puts("Anagrams for given words '#{word}' is: ")
+    puts("Anagrams for given word '#{word}' is: ")
     puts((anagram_result * ' '))
   end
 end
