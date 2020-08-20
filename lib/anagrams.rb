178749c3e36d37ac394a5b5b0f2cbe184761f05d
zzokki81 <zoran.rebic@outlook.com>
2020-08-20 10:49:17 +0200

    Change hash var name

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,25 +1,25 @@
 class Anagrams
   def initialize(dictionary)
     @dictionary = dictionary
-    @hash = Hash.new { |h, k| h[k] = [] }
+    @anagrams_hash = Hash.new { |h, k| h[k] = [] }
     words_dictionary_list.group_by do |word|
       sorted_word = word_hash(word)
-      @hash[sorted_word] ||= []
-      @hash[sorted_word] << word
+      @anagrams_hash[sorted_word] ||= []
+      @anagrams_hash[sorted_word] << word
     end
   end
 
   def group_anagrams
-    @hash.values
+    @anagrams_hash.values
   end
 
   def find(input_word)
     sorted_word = word_hash(input_word)
-    @hash[sorted_word]
+    @anagrams_hash[sorted_word]
   end
 
   def to_s
-    @hash.values.each { |anagram_block| anagram_block.join(' ') }.to_s
+    @anagrams_hash.values.each { |anagram_block| anagram_block.join(' ') }.to_s
   end
 
   private
